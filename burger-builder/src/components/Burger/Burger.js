fe65ca74b24a40e7c9dc260a9e342cbe9803fc20
Tom North <tnorth93@gmail.com>
2020-06-16 10:49:45 -0700

    started Burger.js as a func component and created Burger.module.css with some media queries

---
+++ burger-builder/src/components/Burger/Burger.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  return();
+
+};
+
+export default burger;
--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -38,7 +38,7 @@ class BurgerIngredient extends Component {
 };
 
 BurgerIngredient.propTypes = {
-  type: PropTypes.string,
+  type: PropTypes.string.isRequired,
 };
 
 export default BurgerIngredient;
