a8182da01b9b06a5fad66ecf18637242b0acc014
Tom North <tnorth93@gmail.com>
2020-06-16 10:14:35 -0700

    creating cases for brea-top, bread-bottom, and meat

---
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './burgerIngredient/burgerIngredient.module.css';
+
+const burgerIngredient = (props) => {
+  return 
+}
+
+export default burgerIngredient;
