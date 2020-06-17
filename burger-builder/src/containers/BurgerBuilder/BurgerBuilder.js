6b0729218ac61cbbd34d2ceee1a10827adbb2a01
Tom North <tnorth93@gmail.com>
2020-06-17 13:05:33 -0700

    added order button

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,6 +24,21 @@ class BurgerBuilder extends Component {
       meat: 0,
     },
     totalPrice: 3,
+    purchasable: false,
+  }
+
+  updatePurchaseState () {
+    const ingredients = {
+      ...this.state.ingredients
+    };
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0)
+    this.setState({purchasable: sum > 0});  
   }
 
   addIngredientHandler = (type) => {
@@ -69,6 +84,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice}/> 
       </Aux>
     );
