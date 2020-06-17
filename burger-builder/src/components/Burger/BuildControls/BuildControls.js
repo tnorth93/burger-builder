616d1ceb5a1e0abc8ac8c11e926c78c79480f43b
Tom North <tnorth93@gmail.com>
2020-06-17 13:51:34 -0700

    added handler and state for purchasable burger

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -21,7 +21,7 @@ const buildControls = (props) => (
         removed={()=> props.ingredientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]} />
     ))}
-    <button>SUBMIT ORDER</button>
+    <button className={classes.OrderButton}>SUBMIT ORDER</button>
   </div>
 )
 
