0333cec7a551f8a1ebc6174fc384e83a49ade8a0
Tom North <tnorth93@gmail.com>
2020-06-18 12:03:46 -0700

    created backdrop and backdrop styling

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,7 @@ const buildControls = (props) => (
     <button 
       className={classes.OrderButton}
       disabled={!props.purchasable}
-      onClick={props.ordered}> SUBMIT ORDER </button>
+      onClick={props.ordered}> ORDER NOW </button>
   </div>
 )
 
