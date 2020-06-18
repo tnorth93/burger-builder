9a6e558e1850e8c33af970e824a4968a3625ab5d
Tom North <tnorth93@gmail.com>
2020-06-18 14:08:20 -0700

    inital setup of toolbar folders/files

---
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const toolbar = (props) => (
+  <header>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      <ul></ul>
+    </nav>
+  </header>
+)
+
+export default toolbar;
