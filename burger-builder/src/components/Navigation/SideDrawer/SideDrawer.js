ed4333a4b1b4f5555bff2cc72103359fdf349d27
Tom North <tnorth93@gmail.com>
2020-06-19 10:59:36 -0700

    created sidedrawer folder and file, initial setup of component

---
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+const sideDrawer = (props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default sideDrawer;
