31ff661ded61331f8f67b8aa9b66a9213803a025
Tom North <tnorth93@gmail.com>
2020-06-19 10:50:05 -0700

    added navitems to toolbar js

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
-  <ul>
-    <li><a href="/">A Link</a></li>
-  </ul>
+  <li className={classes.NavigationItem}>
+    <a
+      href={props.link}
+      className={props.active ? classes.active : null}>{props.children}</a>
+  </li>
 );
 
 export default navigationItem;
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
