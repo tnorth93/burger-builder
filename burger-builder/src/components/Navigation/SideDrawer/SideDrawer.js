410e27d44b8d935e97221e37633c7f0a20d18f67
Tom North <tnorth93@gmail.com>
2020-06-19 13:00:09 -0700

    sidedrawer styling

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from "../NavigationItems/NavigationItems";
+import navigationItem from '../NavigationItems/NavigationItem/NavigationItem';
 
 const sideDrawer = (props) => {
   return (
     <div>
-
+      <Logo/>
+      <nav>
+        <NavigationItems/>
+      </nav>
     </div>
   );
 };
