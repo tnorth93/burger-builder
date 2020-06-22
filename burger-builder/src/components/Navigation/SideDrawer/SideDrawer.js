efb785688912f3bcec08ac68ad13c4d13862a8b7
Tom North <tnorth93@gmail.com>
2020-06-22 09:25:38 -0700

    added backdrop to side drawer

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo/>
+    <Aux>
+      <Backdrop show/>
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
