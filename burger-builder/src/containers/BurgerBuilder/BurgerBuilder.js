0f63f1b785eff07d63080926a203e545e5b08fed
Tom North <tnorth93@gmail.com>
2020-06-24 17:03:35 -0700

    added loading spinner to modal when order is submitted

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,7 +96,9 @@ class BurgerBuilder extends Component {
       },
       deliveryMethod: 'ASAP',
     }
-    axios.post('/orders.json');
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
   }
 
   render () {
