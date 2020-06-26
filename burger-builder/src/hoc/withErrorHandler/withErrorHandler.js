a421739a4debcad7b2e0697e2d9502ea1c762df4
Tom North <tnorth93@gmail.com>
2020-06-25 17:00:28 -0700

    added error to witherrorhandler and converted to class based component

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../../hoc/Auxiliary/Auxiliary';
 
-const withErrorHandler = (WrappedComponent) => {
-  return (props) => {
-    return (
-      <Aux>
-        <Modal show>
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+  componentDidMount () {
+    axios.interceptors.response.use(null, error => {
+      this.setState({error: error});
+    });
+  }
+
+    render() {
+      return (
+        <Aux>
+          <Modal show>
           Something didn't work
-        </Modal>
-        <WrappedComponent {...props} />
-      </Aux>
-    );
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      )
+    }
   }
 }
 
