bc47f6cb4b3769d71aae7a03bfcd95b85e11861e
Tom North <tnorth93@gmail.com>
2020-08-20 16:11:46 -0700

    added buttons to checkout

--- burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const checkoutSummary = (props) => {
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
 
+const checkoutSummary = (props) => {
+  return (
+    <div>
+      <h1> Eatin' good in the neighborhood</h1>
+      <div style={{width: '300px', height: '300px', margin: 'auto'}}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger">Cancel</Button>
+      <Button btnType="Success">Continue</Button>
+    </div>
+  )
 }
 
 export default checkoutSummary;
