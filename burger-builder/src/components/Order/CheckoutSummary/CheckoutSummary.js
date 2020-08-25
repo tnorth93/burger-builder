28a688ce0f6eac1b95439944159087189b4ab6db
Tom North <tnorth93@gmail.com>
2020-08-24 17:25:00 -0700

    added state and checkoutsummary comoonent to Checkout.js

--- burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.module.css';
 
 const checkoutSummary = (props) => {
   return (
-    <div>
+    <div className={classes.CheckoutSummary}>
       <h1> Eatin' good in the neighborhood</h1>
       <div style={{width: '300px', height: '300px', margin: 'auto'}}>
         <Burger ingredients={props.ingredients} />
