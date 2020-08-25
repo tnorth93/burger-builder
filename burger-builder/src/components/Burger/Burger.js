280fef7773e5fdd242161de153ea0367a38c1445
Tom North <tnorth93@gmail.com>
2020-08-25 12:30:39 -0700

    added checkoutcancelled and checkoutcontinued handlers to checkout.js

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -114,6 +114,7 @@ class BurgerBuilder extends Component {
     //   .catch(error => {
     //     this.setState({loading: false, purchasing: false})
     //   });
+    this.props.history.push('/checkout')
   }
 
   render () {
