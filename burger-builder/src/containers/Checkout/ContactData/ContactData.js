9849af7172c85a1e7c1f26c6892d0d8d48da6f85
Tom North <tnorth93@gmail.com>
2020-08-25 17:51:15 -0700

    spinner and form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
   state = {
@@ -42,16 +43,20 @@ class ContactData extends Component {
   }
 
   render () {
+    let form = (<form>
+      <input className={classes.Input} type="text" name="name" placeholder="Name" />
+      <input className={classes.Input} type="email" name="email" placeholder="Email" />
+      <input className={classes.Input} type="text" name="street" placeholder="Street" />
+      <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+      <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+    </form>);
+    if (this.state.loading) {
+      form = <Spinner />
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+        {form}
       </div>
     )
   }
