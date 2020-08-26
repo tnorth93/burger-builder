32cae44387657f7810dcca8150ec11f3c2452c80
Tom North <tnorth93@gmail.com>
2020-08-26 15:33:27 -0700

    fixed active navlinks

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './NavigationItem.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, BrowserRouter as Router } from 'react-router-dom';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <NavLink
-      to={props.link}>{props.children}</NavLink>
+    <Router>
+      <NavLink
+        to={props.link}
+        exact={props.exact}
+        activeClassName={classes.active}>{props.children}</NavLink>
+    </Router>
   </li>
 );
 
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -5,7 +5,7 @@ import Order from '../../components/Order/Order';
 class Orders extends Component {
   render () {
     return (
-      
+      <div></div>
     );
   }
 }
