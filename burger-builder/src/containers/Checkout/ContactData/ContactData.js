4914063d61a01a4afbf2e840e07939fec08cd1bb
Tom North <tnorth93@gmail.com>
2020-08-25 17:09:21 -0700

    more styling for form

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.module.css';
 
 class ContactData extends Component {
   state = {
@@ -14,13 +15,13 @@ class ContactData extends Component {
 
   render () {
     return (
-      <div>
+      <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
         <form>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postal" placeholder="Postal Code" />
+          <input className={classes.Input} type="text" name="name" placeholder="Name" />
+          <input className={classes.Input} type="email" name="email" placeholder="Email" />
+          <input className={classes.Input} type="text" name="street" placeholder="Street" />
+          <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
           <Button btnType="Success">ORDER</Button>
         </form>
       </div>
