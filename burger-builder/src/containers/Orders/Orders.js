451001e3b725b8bffa767b106ca33eebc28e4b59
Tom North <tnorth93@gmail.com>
2020-09-01 13:19:36 -0700

    outputting orders correctly with ingredients and price

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -30,11 +30,16 @@ class Orders extends Component {
   render () {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price} />
+        ))}
+        
       </div>
     );
   }
 }
 
-export default withErrorHandler(Orders,axios);
+export default withErrorHandler(Orders, axios);
