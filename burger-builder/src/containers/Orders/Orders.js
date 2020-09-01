9dcac6df9912a9957d57a5db33468d029cdaece6
Tom North <tnorth93@gmail.com>
2020-09-01 12:24:33 -0700

    added axios routing for orders, setup componentdidmount in orders

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        this.setState({loading: false});
+      })
+      .catch(err => {
+        this.setState({loading: false});
+      })
+  }
+
   render () {
     return (
       <div>
