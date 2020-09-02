952851f92f73f35b85cacece621efcc4260452d3
Tom North <tnorth93@gmail.com>
2020-09-02 11:48:05 -0700

    imported Input into contactdata.js, refactored based on changes

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
@@ -44,13 +45,14 @@ class ContactData extends Component {
   }
 
   render () {
-    let form = (<form>
-      <input className={classes.Input} type="text" name="name" placeholder="Name" />
-      <input className={classes.Input} type="email" name="email" placeholder="Email" />
-      <input className={classes.Input} type="text" name="street" placeholder="Street" />
-      <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-      <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-    </form>);
+    let form = (
+      <form>
+        <Input type="text" name="name" placeholder="Name" />
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="text" name="street" placeholder="Street" />
+        <Input type="text" name="postal" placeholder="Postal Code" />
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+      </form>);
     if (this.state.loading) {
       form = <Spinner />
     }
