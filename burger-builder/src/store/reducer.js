4fb2d4411b9b82b5c9d89e3c35ea73b1c691160b
Tom North <tnorth93@gmail.com>
2020-09-22 15:01:43 -0700

    added actiontypes to reducer, setup default

--- burger-builder/src/store/reducer.js
+++ burger-builder/src/store/reducer.js
@@ -6,7 +6,18 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
 
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+
+      };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
