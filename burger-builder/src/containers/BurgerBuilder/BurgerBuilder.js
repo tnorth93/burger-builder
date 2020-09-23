6a051ad21c8219dc33bb615489c756ac12b498df
Tom North <tnorth93@gmail.com>
2020-09-23 12:00:16 -0700

    refactored checkout.js

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,7 +17,6 @@ class BurgerBuilder extends Component {
   //   this.state = {...}
   // }
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -54,19 +53,7 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-    }
-
-    queryParams.push('price=' + this.props.price);
-    const queryString = queryParams.join('&');
-
-        this.props.history.push({
-          pathname: '/checkout',
-          search: '?' + queryString,
-        });
+    this.props.history.push('/checkout');
   }
 
   render () {
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from '../Checkout/ContactData/ContactData';
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    price: 0,
-  }
-
-  componentWillMount() {
-    const query = new URLSearchParams(this.props.location.search);
-    const ingredients = {};
-    let price = 0;
-    for (let param of query.entries()) {
-      // ['salad', '1']
-      if (param[0] === 'price') {
-        price = param[1];
-      } else {
-        ingredients[param[0]] = +param[1];
-      }
-    }
-    this.setState({ingredients: ingredients, totalPrice: price});
-  }
-
+  
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   }
@@ -51,4 +33,11 @@ class Checkout extends Component {
   }
 }
 
-export default Checkout;
+const mapStateToProps = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  }
+}
+
+export default connect(mapStateToProps)(Checkout);
