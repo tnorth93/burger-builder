8140f4c7d9bba42753001cf73523f9ce9bd3ddae
Tom North <tnorth93@gmail.com>
2020-10-05 12:52:04 -0700

    fixed bug with async summary

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -16,12 +16,18 @@ class Checkout extends Component {
   }
 
   render () {
-    return (
-      <div>
+    let summary = <Redirect to="/"/>
+    if (this.props.ings) {
+      summary = (
         <CheckoutSummary
           ingredients={this.props.ings}
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}/>
+      );
+    }
+    return (
+      <div>
+        {summary}
         <Route
           path={this.props.match.path + '/contact-data'}
           // component={ContactData}
