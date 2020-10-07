7c9b367890558613f15a457ec8039c22a92e132d
Tom North <tnorth93@gmail.com>
2020-10-06 20:04:47 -0700

    added loading spinner to orders, refactored render for redux

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -5,6 +5,7 @@ import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
 
@@ -13,15 +14,19 @@ class Orders extends Component {
   }
 
   render () {
-    return (
-      <div>
-        {this.state.orders.map(order => (
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = this.props.orders.map(order => (
           <Order
             key={order.id}
             ingredients={order.ingredients}
             price={order.price} />
-        ))}
-        
+      ))
+    };
+
+    return (
+      <div>
+        {orders}
       </div>
     );
   }
