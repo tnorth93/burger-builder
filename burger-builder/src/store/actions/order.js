222c32677132dcdbc0fa85e644917e73c47a505d
Tom North <tnorth93@gmail.com>
2020-10-06 19:41:42 -0700

    action creators setup for orders page

--- burger-builder/src/store/actions/order.js
+++ burger-builder/src/store/actions/order.js
@@ -42,3 +42,28 @@ export const purchaseInit = () => {
     
   };
 }
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders,
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error,
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START,
+  };
+};
+
+export const fetchOrders = () => {
+  axios.get
+};
+
