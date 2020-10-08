c2018a1bd8ba69aba15cd46cdae6c3c45d880a33
Tom North <tnorth93@gmail.com>
2020-10-08 12:01:01 -0700

    finished refactor order reducer

--- burger-builder/src/store/reducers/order.js
+++ burger-builder/src/store/reducers/order.js
@@ -21,28 +21,38 @@ const purchaseBurgerSuccess = (state, action) => {
     loading: false,
     purchased: true,
     orders: state.orders.concat(newOrder)
-  }),
+  });
+};
+
+const purchaseBurgerFail = (state, action) => {
+  return updateObject(state, { loading: false });
+};
+
+const fetchOrdersStart = (state, action) => {
+  return updateObject(state, { loading: true });
+};
+
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    orders: action.orders,
+    loading: false,
+  });
+};
+
+const fetchOrdersFail = (state, action) => {
+  return updateObject(state, { loading: false });
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
     case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
-    case actionTypes.PURCHASE_BURGER_SUCCESS:
-      
-    case actionTypes.PURCHASE_BURGER_FAIL:
-      return updateObject(state, { loading: false });
-    case actionTypes.FETCH_ORDERS_START:
-      return updateObject(state, { loading: true });
-    case actionTypes.FETCH_ORDERS_SUCCESS:
-      return updateObject(state, {
-        orders: action.orders,
-        loading: false,
-      });
-    case actionTypes.FETCH_ORDERS_FAIL:
-      return updateObject(state, { loading: false });
-    default:
-      return state;
+    case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+    case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+    case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
+    default: return state;
   }
 }
 
