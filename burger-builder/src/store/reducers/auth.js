7eac4613f16cf2dd045208d49568d807900af0a1
Tom North <tnorth93@gmail.com>
2020-10-13 14:35:35 -0700

    added auth success and auth fail cases and helper functions

--- burger-builder/src/store/reducers/auth.js
+++ burger-builder/src/store/reducers/auth.js
@@ -10,11 +10,29 @@ const initialState = {
 
 const authStart = (state, action) => {
   return updateObject(state, {error: null, loading: true});
+};
+
+const authSuccess = (state, action) => {
+  return updateObject(state, {
+    token: action.idToken,
+    userId: action.userId,
+    error: null,
+    loading: false,
+  });
+};
+
+const authFail = (state, action) => {
+  return updateObject(state, {
+    error: action.error,
+    loading: false,
+  })
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state, action);
+    case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+    case actionTypes.AUTH_FAIL: return authFail(state, action);
     default:
       return state;
   }
