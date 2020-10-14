3c85d5126628aae3ce186df24357003d1b54cd65
Tom North <tnorth93@gmail.com>
2020-10-14 15:16:44 -0700

    error is displayed when trying to authenticate unsuccessfully

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -119,8 +119,17 @@ class Auth extends Component {
       form = <Spinner />
     }
 
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage =  (
+      <p>{this.props.error.message}</p>
+      )
+    };
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType='Success'>Submit</Button>
@@ -136,6 +145,7 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
+    error: state.auth.error,
   };
 };
 
