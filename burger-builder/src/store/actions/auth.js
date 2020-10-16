296ff07f803c111ae190b19f82459f75568db0f1
Tom North <tnorth93@gmail.com>
2020-10-16 14:45:50 -0700

    logic for retaining authentication status with valid token

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -24,7 +24,8 @@ const authFail = (error) => {
 
 export const logout = () => {
   localStorage.removeItem('token');
-  localStorage.removeItem('expirationTime');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   }
@@ -56,6 +57,7 @@ export const auth = (email, password, isSignup) => {
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
         localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -71,4 +73,22 @@ export const setAuthRedirectPath = (path) => {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path,
   }
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+        const expirationDate = new Date(localStorage.getItem('expirationDate'));
+        if (expirationDate > new Date()) {
+          const userId = localStorage.getItem('userId');
+          dispatch(authSuccess(token, userId));
+          dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+        } else {
+            dispatch(logout());
+      };
+    };
+  };
 };
