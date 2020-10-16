8b8f0d4d8aa1cdcad322eec37b4598b9b92d3619
Tom North <tnorth93@gmail.com>
2020-10-16 13:58:55 -0700

    storing token and expiration date in localStorage

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -51,7 +51,7 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
-        const expirationDate = new Date() + response.data.expiresIn * 1000;
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
         localStorage.setItem('expirationDate', expirationDate);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
