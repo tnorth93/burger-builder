c150df8b236155b138259bd6438f635986a2b7e4
Tom North <tnorth93@gmail.com>
2020-11-20 16:00:04 -0800

    connected saga to index.js

--- burger-builder/src/store/sagas/auth.js
+++ burger-builder/src/store/sagas/auth.js
@@ -2,11 +2,11 @@ import { put } from 'redux-saga/effects';
 
 import * as actionTypes from './actionTypes';
 
-function* logout(action) {
-  localStorage.removeItem('token');
-  localStorage.removeItem('expirationDate');
-  localStorage.removeItem('userId');
-  put({
+export function* logoutSaga(action) {
+  yield localStorage.removeItem('token');
+  yield localStorage.removeItem('expirationDate');
+  yield localStorage.removeItem('userId');
+  yield put({
     type: actionTypes.AUTH_LOGOUT
   })
 }
