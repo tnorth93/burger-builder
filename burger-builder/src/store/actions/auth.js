29ed4d8e5ac586652296746a16b997010572a062
Tom North <tnorth93@gmail.com>
2020-11-20 16:49:38 -0800

    more logic for checkauthtimeoutsaga

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -35,10 +35,9 @@ export const logoutSucceed = () => {
 };
 
 export const checkAuthTimeout = (expirationTime) => {
-  return dispatch => {
-    setTimeout(() => {
-      dispatch(logout());
-    }, expirationTime * 1000);
+  return {
+    type: actionTypes.AUTH_CHECK_TIMEOUT,
+    expirationTime: expirationTime,
   };
 };
 
