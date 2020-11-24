99711744bb1d07bb4505d1810c7d95b4637a92e1
Tom North <tnorth93@gmail.com>
2020-11-23 17:53:15 -0800

    refactored auth check state to use sagas

--- burger-builder/src/store/sagas/auth.js
+++ burger-builder/src/store/sagas/auth.js
@@ -1,4 +1,4 @@
-import { delay, put } from 'redux-saga';
+import { delay, put } from 'redux-saga/effects';
 import axios from 'axios';
 
 import * as actions from '../actions/index';
@@ -39,4 +39,20 @@ export function* authUserSaga(action) {
     } catch (error) {
         yield put(actions.authFail(error.response.data.error));
     }
+}
+
+export function* authCheckStateSaga(action) {
+    const token =  yield localStorage.getItem('token');
+    if (!token) {
+      yield put(actions.logout());
+    } else {
+        const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
+        if (expirationDate <= new Date()) {
+          yield put(actions.logout());
+        } else {
+            const userId = yield localStorage.getItem('userId');
+            yield put(actions.authSuccess(token, userId));
+            yield put(actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+      };
+    };
 }
