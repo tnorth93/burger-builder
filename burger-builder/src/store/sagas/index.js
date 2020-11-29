e7fd9f27334308fc35c85c3ccc1083a301989937
Tom North <tnorth93@gmail.com>
2020-11-28 18:18:33 -0800

    syntax error

--- burger-builder/src/store/sagas/index.js
+++ burger-builder/src/store/sagas/index.js
@@ -8,7 +8,7 @@ export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
 
 export function* watchBurgerBuilder() {
