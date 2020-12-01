fe1afcfc475100c673f490b9f7a053b46f0bfa65
Tom North <tnorth93@gmail.com>
2020-11-30 18:13:13 -0800

    refactored fetchOrdersSaga from previous logic to use sagas

--- burger-builder/src/store/sagas/order.js
+++ burger-builder/src/store/sagas/order.js
@@ -14,5 +14,19 @@ export function* purchaseBurgerSaga(action) {
 }
 
 export function* fetchOrdersSaga(action) {
-  
+    yield put(actions.fetchOrdersStart());
+    const queryParams = '?auth=' + action.token + '&orderBy="userId"&equalTo="' + action.userId + '"';
+    try {
+      const response = yield axios.get('/orders.json' + queryParams)
+      const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        yield put (actions.fetchOrdersSuccess(fetchedOrders));
+    } catch (error) {
+        yield put(actions.fetchOrdersFail(error))
+    }
 }
