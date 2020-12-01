9e134144956ee09eaf475928b0434d9119715b24
Tom North <tnorth93@gmail.com>
2020-11-30 17:10:08 -0800

    finished watchOrder generator function

--- burger-builder/src/store/sagas/index.js
+++ burger-builder/src/store/sagas/index.js
@@ -3,6 +3,7 @@ import { takeEvery } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth';
 import { initIngredientsSaga } from './burgerBuilder';
+import { purchaseBurgerSaga } from './order';
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
@@ -15,6 +16,6 @@ export function* watchBurgerBuilder() {
   yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
 
-export function *watchOrder() {
-  yield takeEvery(actionTypes.PURCHASE_BURGER)
+export function* watchOrder() {
+  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
 }
