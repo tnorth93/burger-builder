0ffc882f20d9c7a3007d4c7c3d5281ce0925ac00
Tom North <tnorth93@gmail.com>
2020-12-09 15:02:17 -0800

    refactored App.js to prepare for hooks

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import asyncComponent from './hoc/asyncComponent/asyncComponent';
@@ -20,12 +20,11 @@ const asyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth')
 });
 
-class App extends Component {
-  componentDidMount() {
+const app = (props) => {
+  useEffect(() => {
     this.props.onTryAutoSignup();
-  }
+  }, []);
 
-  render() {
     let routes = (
       <Switch>
         <Route path="/auth" component={asyncAuth} />
@@ -34,7 +33,7 @@ class App extends Component {
       </Switch>
     );
 
-    if (this.props.isAuthenticated) {
+    if (props.isAuthenticated) {
       routes = (
         <Switch>
           <Route path="/checkout" component={asyncCheckout} />
@@ -55,7 +54,7 @@ class App extends Component {
       </div>
     );
   }
-}
+
 
 const mapStateToProps = state => {
   return {
@@ -69,4 +68,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
