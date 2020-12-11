9f6af525ca35f3fa5751c62140f0b6a301960a5e
Tom North <tnorth93@gmail.com>
2020-12-11 15:44:37 -0800

    refactored burgeringredients.js to use hooks

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './BurgerIngredient.module.css';
 
-class BurgerIngredient extends Component {
-  render() {
+const BurgerIngredient = props => {
     let ingredient = null;
-    switch (this.props.type) {
+    switch (props.type) {
       case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
@@ -34,7 +33,6 @@ class BurgerIngredient extends Component {
     };
   
     return ingredient;
-  }
 };
 
 BurgerIngredient.propTypes = {
